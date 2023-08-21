4d476c89525d092684068094d5d97191200626fd
Gi5lund <mortengislund@gmail.com>
2023-08-21 13:45:39 +0200

    init

---
+++ src/main/java/demo1/day1demo1/Day1demo1Application.java
@@ -0,0 +1,15 @@
+package demo1.day1demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1demo1Application
+	{
+
+		public static void main(String[] args)
+			{
+				SpringApplication.run(Day1demo1Application.class, args);
+			}
+
+	}
---
+++ src/test/java/demo1/day1demo1/Day1demo1ApplicationTests.java
@@ -0,0 +1,15 @@
+package demo1.day1demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1demo1ApplicationTests
+	{
+
+		@Test
+		void contextLoads()
+			{
+			}
+
+	}
